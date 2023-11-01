db0555a455092f070cfee3807ff201c95f60dd1e
Gabrielle P <114810027+Gaby269@users.noreply.github.com>
2023-11-01 21:10:34 +0100

    Update day1_part2.py

--- 2018/Day1/day1_part2.py
+++ 2018/Day1/day1_part2.py
@@ -9,7 +9,7 @@ name_file = sys.argv[1]         #name of file witch use
 
 # Vairable d'initialisation
 freq_courante = 0
-frequence_vue = set()  # Use a set to store frequencies that have been seen
+freq_vue = set()  # Use a set to store frequencies that have been seen
 
 # Liste des fréquence à partir du fichier en prenant les negatif comme des -
 liste_freq = [int(line.strip()) for line in open(name_file)]
@@ -20,9 +20,9 @@ while True:
         freq_courante += freq
 
         # Si la fréquence est présente deux fois
-        if freq_courante in frequence_vue:
+        if freq_courante in freq_vue:
             print("Première fréquence qui aparait 2 fois :", freq_courante)
             exit()  # On sort du programme
 
         # Ajouter la frequence vue au set
-        frequence_vue.add(freq_courante)
+        freq_vue.add(freq_courante)
