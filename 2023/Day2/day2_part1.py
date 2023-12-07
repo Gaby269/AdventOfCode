b1f9322b4daa244decb704b17760567a0cc9b259
Gabrielle P <114810027+Gaby269@users.noreply.github.com>
2023-12-08 00:00:04 +0100

    Update day2_part1.py

--- 2023/Day2/day2_part1.py
+++ 2023/Day2/day2_part1.py
@@ -14,39 +14,64 @@ file = open(name_file, "r")
 lines = file.readlines()
 
 #READING EACH LINE
-base = [12,13,14]
+base = [12,14,13]
 couleurs = ['rouge', 'bleu', 'vert']
-
-chaine_num = ""
+liste_num = []
+couleurs = []
 somme_num = 0
 dico = {}
 for line in lines :                 #for each line
-  print(line)
+  print("\n\nline : ", line)
   jeux = line.split(':')
-  print(jeux)
   cubes = jeux[1].split(';')
-  print(cubes)
   dico[jeux[0]] = []
   for c in cubes :
     lance = c.split(',')
-    print(lance)
-    dico[jeux[0]].append(lance)
-print(dico)
+    print("lance : ", lance)
+    for la in lance :
+      coul = la.strip().split(" ")
+      couleurs.append([coul[1], coul[0]])
+      
+    print("couleurs : ", couleurs)
+    dico[jeux[0]].append(couleurs)
+    couleurs = []
+    
+    
+    
+print("\n\ndico : ", dico)
 
 for cle in dico.keys() :
-  print(cle)
+  print("\nClé : ", cle)
   print(dico[cle])
   for values in dico[cle] :
-    print(values)
+    print("Values : ", values)
     for v in values :
-      print(v)
-      v = v.strip()
-      s = v.split(" ")
-      print(s)
-      if (s[1] == 'red') :
-        if s[0] > base[0] :
-          break
-  
+      print(" V : ", v)
+      if ('red' in v[0]) :
+        if int(v[1]) > base[0] :
+          if (int(cle.strip().split(" ")[1]) not in liste_num) :
+            liste_num.append(int(cle.strip().split(" ")[1]))
+            print(liste_num)
+            continue # on passe à au v suivant
+      if ('green' in v[0]) :
+        if int(v[1]) > base[1] :
+          if (int(cle.strip().split(" ")[1]) not in liste_num) :
+            liste_num.append(int(cle.strip().split(" ")[1]))
+            print(liste_num)
+            continue
+      if ('blue' in v[0]) :
+        if int(v[1]) > base[2] :
+          if (int(cle.strip().split(" ")[1]) not in liste_num) :
+            liste_num.append(int(cle.strip().split(" ")[1]))
+            print(liste_num)
+            continue # on passe à au v suivant
+    
+    
+    print(liste_num)
+    if liste_num != [] :
+      for li in liste_num :
+        somme_num+= li
+      liste_num = []
 
 
 
